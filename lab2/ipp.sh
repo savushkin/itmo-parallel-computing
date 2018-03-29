a552d98c23dc538c66429569392f46a557e617e1
Ivan Savushkin <ivan@savushkin.me>
2018-03-29 15:53:13 +0300

    build setup

---
+++ lab2/ipp.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+source /opt/intel/mkl/bin/mklvars.sh intel64 lp64
---
+++ lab2/mkl.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+# source /opt/intel/compilers_and_libraries/linux/ipp/bin/ippvars.sh -arch intel64 -platform linux
+source /opt/intel/mkl/bin/mklvars.sh intel64 lp64
