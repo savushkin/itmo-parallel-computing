0d324dfddf570add8fa89b14a1429b33ec14d9d8
Ivan Savushkin <ivan@savushkin.me>
2018-04-20 06:44:37 +0300

    lab3 csv

--- create-csv-file.sh
+++ create-csv-file.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 BIN='./lab1'
-# N1=$(./find-size.sh -b ./lab1-seq -s 100 -t 10)
-N1=900
+N1=$(./find-size.sh -b ./bin/lab1-seq -s 100 -t 10)
+# N1=900
 echo "N1 = ${N1}"
-# N2=$(./find-size.sh -b ./lab1-seq -s 100 -t 2000)
-N2=5400
+N2=$(./find-size.sh -b ./bin/lab1-seq -s 100 -t 2000)
+# N2=5400
 echo "N2 = ${N2}"
 DELTA=$(expr $(expr ${N2} - ${N1}) / 10)
 echo "DELTA = ${DELTA}"
@@ -27,14 +27,14 @@ function write_csv() {
 
 write_csv ${N1} ${N2} ${DELTA} lab1-seq seq.csv
 write_csv ${N1} ${N2} ${DELTA} lab1-par-2 lab1-par-2.csv
-#write_csv ${N1} ${N2} ${DELTA} lab1-par-4 lab1-par-4.csv
-#write_csv ${N1} ${N2} ${DELTA} lab1-par-5 lab1-par-5.csv
-#write_csv ${N1} ${N2} ${DELTA} lab1-par-8 lab1-par-8.csv
+write_csv ${N1} ${N2} ${DELTA} lab1-par-4 lab1-par-4.csv
+write_csv ${N1} ${N2} ${DELTA} lab1-par-5 lab1-par-5.csv
+write_csv ${N1} ${N2} ${DELTA} lab1-par-8 lab1-par-8.csv
 
-#write_csv ${N1} ${N2} ${DELTA} lab2-par lab2-par-2.csv 1
-#write_csv ${N1} ${N2} ${DELTA} lab2-par lab2-par-2.csv 2
-#write_csv ${N1} ${N2} ${DELTA} lab2-par lab2-par-4.csv 4
-#write_csv ${N1} ${N2} ${DELTA} lab2-par lab2-par-5.csv 5
+write_csv ${N1} ${N2} ${DELTA} lab2-par lab2-par-2.csv 1
+write_csv ${N1} ${N2} ${DELTA} lab2-par lab2-par-2.csv 2
+write_csv ${N1} ${N2} ${DELTA} lab2-par lab2-par-4.csv 4
+write_csv ${N1} ${N2} ${DELTA} lab2-par lab2-par-5.csv 5
 
 write_csv ${N1} ${N2} ${DELTA} lab3-par-static-1-1 lab3-par-static-1-1.csv
 write_csv ${N1} ${N2} ${DELTA} lab3-par-static-1-2 lab3-par-static-1-2.csv
---
+++ prepare-system.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+systemctl stop teamviewerd.service wicd.service nginx.service avahi-daemon.service avahi-daemon.socket libvirt-guests.service libvirtd.service lightdm.service postgresql.service
